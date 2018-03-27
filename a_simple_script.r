a603d2c62b5ca1ace9f0e023508455c65dffad44
eotp <krois@zedat.fu-berlin.de>
2018-03-27 21:59:09 +0200

    add r toy script

---
+++ a_simple_script.r
@@ -0,0 +1,10 @@
+
+## just an R-script
+
+print("Hello World!")
+
+print("I am a simple R Script and do whatever you want me do to")
+
+rv = round(sqrt(1823489372))
+print(paste("For instance, I may compute the square root of 1823489372 for you:", rv)) 
+
